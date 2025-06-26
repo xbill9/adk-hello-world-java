0635702f2fc22b9cec94467d4ba70eb6add43251
xbill <xbill@glitnir.com>
2025-06-26 15:04:34 +0000

    add sample cli call

---
+++ cli.sh
@@ -0,0 +1,4 @@
+. $HOME/adk-hello-world-java/set_env.sh
+
+/usr/bin/env /nix/store/8yqdr7xk055fgqjzhcjdspnf24w70qwp-zulu-ca-jdk-17.0.8.1/bin/java @/tmp/cp_2v1mevgsmoq48vatbga38vlze.argfile agents.multitool.MultiToolAgent
+
